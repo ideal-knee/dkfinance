f5d271f86064807cab1026f54c332ff8483163f1
Dan Kee <dan@dankee.com>
2013-11-28 13:13:42 -0600

    Implement Transaction.create_from_csv_line

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,17 @@ class Transaction < ActiveRecord::Base
   belongs_to :category
 
   def self.create_from_csv_line(line)
-    line
+    parts = line.split(',').map { |part| part.gsub '"', '' }
+
+    if parts.length == 4
+      date, description, amount, category_name = parts
+      date = Date.parse(date)
+      amount = amount.to_f
+      category = Category.find_by(name: category_name) || Category.create(name: category_name)
+      Transaction.create(date: date, description: description, amount: amount, category: category)
+    else
+      Rails.logger.info "Transaction.create_from_csv_line: Can't create transaction from '#{line}'"
+      nil
+    end
   end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 
 describe Transaction do
+  before :each do
+    Category.delete_all
+    Transaction.delete_all
+  end
+
   describe "Transaction.create_from_csv_line" do
-    it "returns what you pass it" do
-      Transaction.create_from_csv_line("asdf").should == "asdf"
+    it "parses csv line and creates a transaction from it" do
+      line = %{"2013-01-02","TRADER JOE'S #503  QPS FRAMINGHAM    MA","-12.96","Groceries"}
+      t = Transaction.create_from_csv_line(line)
+      t.date.should == Date.parse('2013-01-02')
+      t.description.should == "TRADER JOE'S #503  QPS FRAMINGHAM    MA"
+      t.amount.should == -12.96
+      t.category.name.should == 'Groceries'
     end
   end
 end
