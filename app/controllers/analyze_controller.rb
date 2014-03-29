558a130721026125770a16b80f2c5f8fa5a6c0b8
Dan Kee <dan@dankee.com>
2014-03-28 21:46:32 -0500

    Link to month_breakdown.

--- app/controllers/analyze_controller.rb
+++ app/controllers/analyze_controller.rb
@@ -3,18 +3,17 @@ class AnalyzeController < ApplicationController
 
   def month_to_month
     paycheck_category = current_user.categories.find_by(name: 'Paycheck')
-    other_categories = current_user.categories.where.not(name: ['Paycheck', 'Uncategorized'])
+    @categories = current_user.categories.where.not(name: ['Paycheck', 'Uncategorized', 'Education', 'Transfer', 'Interest', 'Rent', 'Gifts', 'Fitness', 'Cash', 'Alcohol', 'Bank Fee', 'Tax Expense'])
     @results = []
-    @categories = other_categories.map { |c| c.name.to_sym }
     12.times do |i|
       date = (Date.today - i.months)
       date_range = date.beginning_of_month..date.end_of_month
       by_category = {}
-      other_categories.each do |category|
-        by_category[category.name.to_sym] = current_user.transactions.where(category_id: category.id, date: date_range).sum(:amount)
+      @categories.each do |category|
+        by_category[category] = current_user.transactions.where(category_id: category.id, date: date_range).sum(:amount)
       end
       @results << {
-        month: date.strftime('%B %Y'),
+        date: date,
         net: current_user.transactions.where(date: date_range).sum(:amount),
         income: current_user.transactions.where(category_id: paycheck_category.id, date: date_range).sum(:amount),
         by_category: by_category
